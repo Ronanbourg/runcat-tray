20eefa1da56b035cd481747fc4e5dce30042f01c
Ronan Thomas <derobian@DESKTOP-4STTIO8-DEBIAN.debianlan>
2025-08-10 01:24:29 +0200

    Fix compatibility with libayatana-appindicator3 by updating pkg-config module in CMakeLists.txt and conditional includes in main.c

---
+++ build/CMakeFiles/runcat.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runcat.dir/main.c.o"
+  "CMakeFiles/runcat.dir/main.c.o.d"
+  "runcat"
+  "runcat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/runcat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
